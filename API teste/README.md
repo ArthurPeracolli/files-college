5f8552fb0418cbd0ccddb72596c7d38d24dff980
ArthurPeracolli <arthur.freitas@sptech.school>
2022-04-23 00:17:14 -0300

    API teste

---
+++ API teste/README.md
@@ -0,0 +1,5 @@
+# Dicas gerais para utilização
+
+Ao utilizar os templates do Header e Footer, COPIEM E COLEM O CÓDIGO, não deem clone e alterem o código, se não quando commitarem vai alterar o código dos templates.
+
+Não se esqueçam do arquivo style.css e do link, sem eles o style do Header e do Footer vão quebrar.
---
+++ API teste/src/models/empresaModel.js
@@ -0,0 +1,38 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI A EMPRESA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM empresa;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI A EMPRESA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM empresa WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nomeFantasia, cnpj, RazaoSocial, telefone, email, senha, rua, bairro, cidade, estado, cep) {
+    console.log("ACESSEI A EMPRESA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO empresa (NomeFantasia, CNPJ, RazaoSocial, Telefone, Email, Senha, Rua, Bairro, Cidade, Estado, CEP) VALUES ('${nomeFantasia}', '${cnpj}', '${RazaoSocial}', '${telefone}', '${email}', '${senha}', '${rua}', '${bairro}', '${cidade}', '${estado}', '${cep}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+};
